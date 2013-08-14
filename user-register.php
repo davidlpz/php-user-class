8a91736861da9504655f76cadeb1b2b311792c59
David López <david@iMac-de-David.local>
2013-08-14 02:41:08 +0200

    Removed autocomplete=off from input tag

--- user-register.php
+++ user-register.php
@@ -2,12 +2,12 @@
 	<?php $user->display_info(); ?>
 	<?php $user->display_errors(); ?>
 	<label for="email">E-mail:</label>
-	<input type="email" id="email" name="email" placeholder="email" autocomplete="off" />
+	<input type="email" id="email" name="email" placeholder="email" />
 	<label for="username">Username:</label>
-	<input type="text" id="username" name="username" placeholder="username" autocomplete="off" />
+	<input type="text" id="username" name="username" placeholder="username" />
 	<label for="password">Password:</label>
-	<input type="password" id="password" name="password" placeholder="password" autocomplete="off" />
+	<input type="password" id="password" name="password" placeholder="password" />
 	<label for="confirm">Password:</label>
-	<input type="password" id="confirm" name="confirm" placeholder="repeat password" autocomplete="off" />
+	<input type="password" id="confirm" name="confirm" placeholder="repeat password" />
 	<input id="button-register" type="submit" name="register" value="Register">
 </form>
